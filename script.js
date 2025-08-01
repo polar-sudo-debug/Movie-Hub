4bcbbf198dba811af15ff3474857f4fbcb8b4225
polar-sudo-debug <polargtyt2@gmail.com>
2025-08-01 13:39:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+async function searchMovies() {
+    const query = document.getElementById('search-bar').value;
+    const results = await fetchMovies(query);
+    displayResults(results);
+}
 
+async function fetchMovies(query) {
+    const response = await fetch(`api/movies.js?query=${query}`);
+    const data = await response.json();
+    return data;
+}
+
+function displayResults(results) {
+    const resultsContainer = document.getElementById('movie-results');
+    resultsContainer.innerHTML = '';
+
+    results.forEach(movie => {
+        const movieItem = document.createElement('div');
+        movieItem.className = 'movie-item';
+        movieItem.innerHTML = `
+            <h2>${movie.title}</h2>
+            <p>${movie.description}</p>
+            <button onclick="watchMovie('${movie.link}')">Watch Now</button>
+        `;
+        resultsContainer.appendChild(movieItem);
+    });
+}
+
+function watchMovie(link) {
+    window.open(link, '_blank');
+}
